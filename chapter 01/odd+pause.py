73be5ba61d890b261c776f154d54b192cbb9a3f0
consul-k <138245769+consul-k@users.noreply.github.com>
2023-10-22 20:40:06 +0300

    Add files via upload

---
+++ chapter 01/odd+pause.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime
+
+odds = list(range(1,60,2))
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print('This minute seems a little odd.')
+    else:
+        print('Not an odd minute.')
+    time.sleep(5)
